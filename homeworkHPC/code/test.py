9fc1a05a92ee5a4e09cd666ff4c8792e0073367a
ningyuma <ningyuma462@gmail.com>
2017-02-16 01:32:13 -0500

    hpc hw first try

---
+++ homeworkHPC/code/test.py
@@ -0,0 +1,14 @@
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+assert comm.size == 2
+
+if comm.rank == 0:
+   sendmsg = 123
+   comm.send(sendmsg, dest=1, tag=11)
+   recvmsg = comm.recv(source=1, tag=22)
+   print "[%02d] Received message: %s" % (comm.rank, recvmsg)
+else:
+   recvmsg = comm.recv(source=0, tag=11)
+   print "[%02d] Received message: %d" % (comm.rank, recvmsg)
+   sendmsg = "Message from 1"
+   comm.send(sendmsg, dest=0, tag=22)
